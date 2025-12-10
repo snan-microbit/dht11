daeaad30abec53816325df8a0e66ec7875c8bc14
snan-microbit <snan@ceibal.edu.uy>
2025-12-10 13:38:07 -0300

    Actualizar main.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-basic.forever(function () {
-    basic.pause(2000)
-    serial.writeValue("Temp", dht11.dht11value(dht11.DHT11Type.TemperatureC, DigitalPin.P0))
-    basic.pause(2000)
-    serial.writeValue("Hum", dht11.dht11value(dht11.DHT11Type.Humidity, DigitalPin.P0))
-})
